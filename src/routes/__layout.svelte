c1e81e96040bdc662d1ceeff4029117c3bc95594
Michail Kalogeras <89188677+MK-XXXVII@users.noreply.github.com>
2022-06-21 23:04:32 +0200

    Add footer component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,3 +5,12 @@
 	<slot />
 </main>
 
+<script>
+	import Footer from '$lib/components/footer.svelte'
+	import '../app.css'
+ </script>
+ 
+ <main class="container max-w-3xl mx-auto px-4 mb-20">
+	<slot />
+ </main>
+ <Footer />
